7701a1becf095a49d3d694eda0f49368fde88963
humyna <hufo125@gmail.com>
2014-10-20 23:08:47 +0800

    java基础：基本类型的说明
    Signed-off-by: humyna <hufo125@gmail.com>

---
+++ src/main/java/info/zoio/tec/java/basics/d141020/basictype/TypeTest.java
@@ -0,0 +1,23 @@
+package info.zoio.tec.java.basics.d141020.basictype;
+
+/**
+ * 基本类型供分为9种:byte/short/int/long/float/double/boolean/char/【void】，每种基本类型都对应一个“包装类”
+ *
+ * 二进制位数：8/16/32/64/32/64/16/8
+ * 类型转换：
+ * 	无损精度转换(byte->short;short->int;char->int;int->long;float->double)——>低位转临近高位
+ * 	损失精度转换(int->float;long->float;long->double);
+ * 	其他转换非法
+ *
+ *
+ * Void - java.lang中的 类
+ * Void 类是一个不可实例化的占位符类，它保持一个对代表 Java 关键字 void 的 Class对象的引用。
+ * VOID - 类 javax.management.openmbean.SimpleType中的静态变量
+ * SimpleType实例描述其 Java 类名称为 java.lang.Void 的值。
+ *
+ * @author humyna
+ *
+ */
+public class TypeTest {
+
+}
